0ef26d3168437552ed6c27c7691750be486c7e4c
enos lyznne <emuthiani26@gmail.com>
2023-12-19 15:36:46 +0300

    :dizzy:  -ui

--- app/page.js
+++ app/page.js
@@ -1,32 +1,90 @@
 // import Image from 'next/image'
-import styles from './page.module.css'
-import Link from "next/link"
-
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { FiSmile, FiGithub } from "react-icons/fi";
 import { SiHomebridge } from "react-icons/si";
+
 export default function Home() {
   return (
     <main className={styles.main}>
       {/* top */}
-     <Link href="/dashbboard">
-     <div className={styles.top}>
-        <h1>Dash <span>/</span></h1>
+
+      <div className={styles.top}>
+        <Link href="/dashbboard">
+          <p>
+            Dash <span>/</span>
+          </p>
+        </Link>
+
+        <a
+          href="https://github.com/lyznne/dashboard"
+          target="_blank"
+          className={styles.github}
+        >
+          <button className={styles.Btn}>
+            <span className={styles.svgContainer}>
+              <svg fill="white" viewBox="0 0 496 512" height="1.6em">
+                <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
+              </svg>
+            </span>
+            <span className={styles.BG}></span>
+          </button>
+        </a>
       </div>
-     </Link>
-      
 
       {/* center  */}
-      <div className={styles.center}>
-        <div className={styles.icon}>
-          <SiHomebridge />
-          <h1>Dashboard </h1>
-          <div>&gt;</div>
+      <div className={styles.component}>
+        <div className={styles.container}>
+          <div className={styles.center}>
+            <div className={styles.icon}>
+              <SiHomebridge className={styles.homeIcon} />
+              <h3>Dashboard </h3>
+            </div>
+
+            <div className={styles.btns}>
+              <Link href="/dashboard">
+                <button className={styles.btnProceed}>Proceed</button>
+              </Link>
+              <Link href="/login">
+                <button className={styles.cssbuttons}>
+                  Get started
+                  <div className={styles.icon}>
+                    <svg
+                      height="24"
+                      width="24"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M0 0h24v24H0z" fill="none"></path>
+                      <path
+                        d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                  </div>
+                </button>
+              </Link>
+            </div>
+          </div>
         </div>
+        <Image
+          src="/analyst.png"
+          alt=""
+          height={500}
+          width={500}
+          className={styles.bg}
+        />
+      </div>
+      {/* footer = github + made by */}
 
-        proceed + login btns
-        </div>
-
-      footer = github + made by
-
+      <Link
+        href="https://github.com/lyznne"
+        target="_blank"
+        className={styles.user}
+      >
+        made by - enos <FiSmile className={styles.smile} />
+      </Link>
     </main>
-  )
+  );
 }
